b5fe131e1cdd2bd07c979d1f066abc6b5fea4b76
Marc Ermshaus <marc@ermshaus.org>
2016-12-15 03:33:13 +0100

    Stop advising users to pipe a curl download directly into php.

--- demos/bootstrap.php
+++ demos/bootstrap.php
@@ -2,10 +2,10 @@
 
 header('Content-Type: text/html; charset=UTF-8');
 
-if ((!$loader = @include __DIR__.'/../../../autoload.php')
-        && (!$loader = @include __DIR__.'/../vendor/autoload.php')
+if (
+    (!$loader = @include __DIR__.'/../../../autoload.php')
+    && (!$loader = @include __DIR__.'/../vendor/autoload.php')
 ) {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
+    die('You must set up the project dependencies, run the following command:'.PHP_EOL.
+        'composer install'.PHP_EOL);
 }
